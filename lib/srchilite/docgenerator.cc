3fb5cb2828835f818e4c6d50d7175b0f908af5ba
Lorenzo Bettini <bettini@dsi.unifi.it>
2011-04-23 17:35:32 +0200

    updated info about the embedding of inputlang

--- lib/srchilite/docgenerator.cc
+++ lib/srchilite/docgenerator.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 1999, 2000, 2002, 2003  Lorenzo Bettini <http://www.lorenzobettini.it>
+ * Copyright (C) 1999-2011  Lorenzo Bettini <http://www.lorenzobettini.it>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
